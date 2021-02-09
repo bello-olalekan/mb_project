5f65a3ee7a0af2cfc54e5da3cd07efae335c6474
bello-olalekan <belloolalekan11@yahoo.com>
2021-02-09 14:56:52 +0100

    New Updates for Heroku Deplyment

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*bxu03)jxqa+3xgfxy7m0n2na9yvp!_mk+cnn-2hmbsg=)7(u!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
